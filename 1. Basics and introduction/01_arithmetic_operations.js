738d0749eaba29580011cf5ad7c66b9f83bef6db
Siyabonga Samson Lukhele <110550090+markuptitan@users.noreply.github.com>
2024-06-21 12:23:20 +0200

    Create  01_arithmetic_operations.js

---
+++ 1. Basics and introduction/01_arithmetic_operations.js
@@ -0,0 +1,8 @@
+// This function adds two integers and returns their value
+
+function add(){
+  return 5 + 4;
+}
+
+// We print the value(result) of the function to the terminal
+console.log(add());
